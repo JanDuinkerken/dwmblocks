d9fc2970f5c1d937f4269b0da8f7df7b7943903e
Jan Duinkerken Rodríguez <jan.duinkerken.rodriguez@gmail.com>
2025-05-26 20:45:18 +0200

    feat: add padding

--- blocks.def.h
+++ blocks.def.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"CPU: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/cpu-freq.sh", 5, 0},
+	{"   CPU: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/cpu-freq.sh", 5, 0},
 
 	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"Updates: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/updates.sh", 350, 0},
+//	{"Updates: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/updates.sh", 350, 0},
 	
 	{"", "sh $HOME/Projects/dwm/dwmblocks/scripts/system-bluetooth.sh", 10, 0},
 
 	{"Bat: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/battery.sh", 10, 0},
 
-	{"", "date '+%b %d (%a) %H:%M'",					5,		0},
+	{"", "echo \"$(date '+%b %d (%a) %H:%M')   \"",					5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
