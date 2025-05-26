da49ae616482da1302850b18b296432198c4366d
Jan Duinkerken Rodríguez <jan.duinkerken.rodriguez@gmail.com>
2025-05-26 20:04:21 +0200

    feat: add scripts and functionalities

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"CPU: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/cpu-freq.sh", 5, 0},
+
+	{"Mem: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"Updates: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/updates.sh", 350, 0},
+	
+	{"", "sh $HOME/Projects/dwm/dwmblocks/scripts/system-bluetooth.sh", 10, 0},
+
+	{"Bat: ", "sh $HOME/Projects/dwm/dwmblocks/scripts/battery.sh", 10, 0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
---
+++ scripts/updates.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sh $HOME/Projects/dwm/dwmblocks/scripts/checkupdates | wc -l
